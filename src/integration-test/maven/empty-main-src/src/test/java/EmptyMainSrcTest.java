042934c99018a6092affc30d7bd55e18b23ac5f0
Peter De Maeyer <peter.de.maeyer@gmail.com>
2025-02-26 21:28:57 +0100

    Added EmptyMainSrcTest + misc amendments

---
+++ src/integration-test/maven/empty-main-src/src/test/java/EmptyMainSrcTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class EmptyMainSrcTest {
+
+	@Test
+	void success() {
+		assertTrue(true);
+	}
+}
